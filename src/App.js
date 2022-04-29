28d2662f82026532fdfed0eea75e59d6464055da
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-04-29 23:14:28 +0200

    add key, .env, useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
+const baseUrl = `https://api.openweathermap.org/data/2.5/onecall?`;
+const apiKey = process.env.REACT_APP_API_KEY;
+
 function App() {
+  const [weatherData, setWeatherData] = useState('');
+  const [value, setValue] = useState("metric");
+
+  const fahrenheit = (e) => {
+    setValue("imperial");
+  };
+  const celsius = (e) => {
+    setValue("metric");
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
